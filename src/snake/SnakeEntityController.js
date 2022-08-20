b2b7c3b32245ce95f2ad71757957e975c5a9caa4
Kunal Kamble <kunalgk1999@gmail.com>
2022-08-20 15:00:37 +0530

    Added support for snake body

--- src/snake/SnakeEntityController.js
+++ src/snake/SnakeEntityController.js
@@ -13,22 +13,35 @@ class SnakeEntity extends Entity {
 export class SnakeEntityController extends EntityController {
   constructor() {
     super();
-    this.entity = new SnakeEntity(0, 0);
+    this.snakeEntities = [
+      new SnakeEntity(0, 0),
+      new SnakeEntity(10, 0),
+      new SnakeEntity(20, 0),
+    ];
+    this.snakeHeadEntity = this.snakeEntities[2];
+    this.entity.subEntities = this.snakeEntities;
     this.direction = Direction.none;
   }
 }
 
 SnakeEntityController.prototype.update = function () {
+  if (this.direction === Direction.none) {
+    return;
+  }
+
   let deltaInXYForDirection = {};
-  deltaInXYForDirection[Direction.none] = [0, 0];
   deltaInXYForDirection[Direction.up] = [0, -SNAKE_ENTITY_SIZE];
   deltaInXYForDirection[Direction.left] = [-SNAKE_ENTITY_SIZE, 0];
   deltaInXYForDirection[Direction.down] = [0, SNAKE_ENTITY_SIZE];
   deltaInXYForDirection[Direction.right] = [SNAKE_ENTITY_SIZE, 0];
 
   const [deltaX, deltaY] = deltaInXYForDirection[this.direction];
-  this.entity.frame.x += deltaX;
-  this.entity.frame.y += deltaY;
+  const newHeadX = this.snakeHeadEntity.frame.x + deltaX;
+  const newHeadY = this.snakeHeadEntity.frame.y + deltaY;
+  const newSnakeHeadEntity = new SnakeEntity(newHeadX, newHeadY);
+  this.snakeEntities.shift();
+  this.snakeEntities.push(newSnakeHeadEntity);
+  this.snakeHeadEntity = newSnakeHeadEntity;
 };
 
 SnakeEntityController.prototype.didKeyDown = function (key) {
